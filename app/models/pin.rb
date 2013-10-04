448de363d992b9f5654de164492659aa92785039
joel <joel@wtx.com.au>
2013-10-03 20:32:25 -0700

    displays larger centered image for portfolio picture

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -9,7 +9,7 @@ class Pin < ActiveRecord::Base
   														 size: { less_than: 5.megabytes }
 
   belongs_to :user
-  has_attached_file :image, styles: { medium: "640x480>"}
+  has_attached_file :image, styles: { medium: "960x720>"}
 
   def image_remote_url=(url_value)
   	self.image = URI.parse(url_value) unless url_value.blank?
